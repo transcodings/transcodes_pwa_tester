2819eeaaa2dce6e86f035dbd4f8d497667fdc898
han55776 <g.han55776@gmail.com>
2025-09-06 17:49:15 -0700

    added files in main

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'public', // ✅ Ensure Vite serves from `public`
+  build: {
+    outDir: '../dist', // place dist outside of public/
+    emptyOutDir: true,
+  },
   server: {
     open: true, // ✅ Automatically opens the browser
     port: 5173, // ✅ Default Vite port
